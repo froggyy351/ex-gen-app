0c8f5c2873b9a064b9b755d85d29d988ff5dec68
Koichi Sakogawa <kouichi19980817@gmail.com>
2025-11-28 16:12:52 +0900

    feat: add sqlite3

--- routes/hello.js
+++ routes/hello.js
@@ -1,33 +1,56 @@
 const express = require('express');
 const router = express.Router();
-const http = require('https');
-const parseString = require('xml2js').parseString;
 
-//GoogleニュースのRSSページへのアクセスとデータ取得
+const sqlite3 = require('sqlite3');
+
+//データベースオブジェクトの取得
+const db = new sqlite3.Database('mydb.db');
+
+//GETアクセスの処理
 router.get('/', (req, res, next) => {
-    var opt = {
-        host: 'news.google.com',
-        port: 443,
-        path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
-    };
-
-    http.get(opt, (res2) => {
-        var body = '';
-        res2.on('data', (data) => {
-            body += data;
-        });
-        res2.on('end', () => {
-            parseString(body.trim(), (err, result) => {
-                console.log(result);
+    db.serialize(() => {
+        //レコードをすべて取り出す
+        db.all("select * from mydata", (err, rows) => {
+            //データベースアクセス完了時の処理
+            if (!err) {
                 var data = {
-                    title: 'Google News',
-                    content: result.rss.channel[0].item
+                    title: 'Hello!',
+                    content: rows
                 };
                 res.render('hello', data);
-            });
+            }
         });
     });
 });
+
+// const http = require('https');
+// const parseString = require('xml2js').parseString;
+
+//GoogleニュースのRSSページへのアクセスとデータ取得
+// router.get('/', (req, res, next) => {
+//     var opt = {
+//         host: 'news.google.com',
+//         port: 443,
+//         path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
+//     };
+
+//     http.get(opt, (res2) => {
+//         var body = '';
+//         res2.on('data', (data) => {
+//             body += data;
+//         });
+//         res2.on('end', () => {
+//             parseString(body.trim(), (err, result) => {
+//                 console.log(result);
+//                 var data = {
+//                     title: 'Google News',
+//                     content: result.rss.channel[0].item
+//                 };
+//                 res.render('hello', data);
+//             });
+//         });
+//     });
+// });
 
 // router.get('/', (req, res, next) => {
 //     var msg = '※何か書いて送信してください。';
