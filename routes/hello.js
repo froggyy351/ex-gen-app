983265ee92ca5d7f9aee4e47b6cbe4b1fb1edf2c
Koichi Sakogawa <kouichi19980817@gmail.com>
2025-12-01 02:33:33 +0900

    feat: add SQL C/R

--- routes/hello.js
+++ routes/hello.js
@@ -20,13 +20,46 @@ router.get('/', (req, res, next) => {
                     title: 'Hello!',
                     content: rows
                 };
-                res.render('hello', data);
+                res.render('hello/index', data);
             }
         });
     });
 });
 
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Hello/add',
+        content: '新しいレコードを入力'
+    }
+    res.render('hello/add', data);
+});
 
+router.post('/add', (req, res, next) => {
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    db.serialize(() => {
+        db.run('Insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+    });
+    res.redirect('/hello');
+});
+
+router.get('/show', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: '/Hello/show',
+                    content: 'id' + id + ' のレコード：',
+                    mydata: row
+                }
+                res.render('hello/show', data);
+            }
+        });
+    });
+});
 
 // const http = require('https');
 // const parseString = require('xml2js').parseString;
